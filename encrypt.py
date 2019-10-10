738a687a9e9b672ee7047c87779f65435656d9f9
aronjschwartz <aschwa2@pdx.edu>
2019-10-09 20:40:18 -0700

    Added code to test encryption/decryption of numbers 2-36 (same as table in the thesis)
    Encrypting is working! Decryption is not yet working

--- encrypt.py
+++ encrypt.py
@@ -11,7 +11,19 @@ import mod
 
 def check_coprimality(a, b):
     return bltin_gcd(a, b) == 1
+
+def get_ascii_list(input_string):
+	out = []
+	for i in input_string:
+		out.append(ord(i))
+	return out
 	
+def get_string_from_ascii(input_list):
+	out = ""
+	for i in input_list:
+		out += chr(i)	
+	return out
+
 #This class accepts 2 prime numbers and generates/stores all associated values to perform the RSA encryption method
 class encryption_set():
 	#Need the original primes to initialize the object
@@ -21,16 +33,18 @@ class encryption_set():
 		self.q = q
 		self.n = p*q
 		self.totient = self.generate_totient(p, q)
-		self.e = self.generate_e(self.totient)
-		self.d = self.generate_d()
+		self.e = 7
+		self.d = 31
+		#self.e = self.generate_e(self.totient)
+		#self.d = self.generate_d()
 		
 	#e must be a value that is: less than n, coprime with the totient, satisfies m^e > n where 'm' is the integer to be transformed
 
 	def generate_e(self, totient):   
 		candidate_found = False
 		while(candidate_found == False):
-			rand_num = random.sample(range(1, self.n), 1)[0]
-			if ((rand_num < self.n) and (check_coprimality(totient, rand_num)) and ((math.pow(127, rand_num) > self.n))): #128 for ascii
+			rand_num = random.sample(range(2, self.n), 1)[0]
+			if ((rand_num < self.n) and (check_coprimality(totient, rand_num))): #and ((math.pow(127, rand_num) > self.n))): #128 for ascii
 				print("CANDIDATE FOUND: ", rand_num)
 				break
 			else:
@@ -65,13 +79,15 @@ class encryption_set():
 	
 def main():
 	#Create an RSA encryption set object
-	RSA_object = encryption_set(13, 7)
-	#Some plain text to test the algorithm
+	RSA_object = encryption_set(2, 19)
 	plain_text = "Aron is rather cool"
+	
 	#List to hold the ascii values for each character in the plain text
 	plain_text_ascii = []
+	
 	#List to hold the transformed ascii value for each character in the cypher text
 	cipher_ascii = []
+	
 	#Var to hold the cipher text equivalent of the plain text
 	cipher_text = ""
 	
@@ -79,17 +95,18 @@ def main():
 	#Var to hold the decyrpted cipher text, which should equal the plain text if functioning properly
 	decrypted_cipher_text = ""
 	
+	
+	
 	RSA_object.to_String()
 	
-	for i in plain_text:
-		plain_text_ascii.append(ord(i))
-		
+	#Obtain a list of ascii values for the plain text
+	plain_text_ascii = get_ascii_list(plain_text)
+	
 	for i in plain_text_ascii:
 		cipher_ascii.append(int(RSA_object.generate_cypher(i)))
 
-	for i in cipher_ascii:
-		cipher_text += chr(i)
-
+	
+	cipher_text = get_string_from_ascii(cipher_ascii)
 	#Test now that decrpyting cipher text results in original plain text
 	for i in cipher_ascii:
 		decrypted_cipher_ascii.append(RSA_object.generate_plain(i))
@@ -101,5 +118,17 @@ def main():
 	print("Plain text: ", plain_text)
 	print("Cipher text: ", cipher_text)
 	print("Decrypted cipher text: ", decrypted_cipher_text)
-
+	
+	val = 9
+	test_list = range(2, 37)
+	encrytped_test_list = []
+	decrypted_test_list = []
+	for i in test_list:
+		print("Encrypting val: ", i, "Result: ", int(RSA_object.generate_cypher(i)))
+		encrytped_test_list.append(int(RSA_object.generate_cypher(i)))
+		
+	for i in encrytped_test_list:
+		print("Decrytping val: ", i, "Result: ", int(RSA_object.generate_plain(i)))
 main()
+
+
