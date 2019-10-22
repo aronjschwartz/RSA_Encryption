8d57366d369239a109a7bcd606794923af28f46e
aronjschwartz <aschwa2@pdx.edu>
2019-10-21 19:04:47 -0700

    Migrated test code to test.py so encrypt.py just holds the RSA object functionality.  Tested test.py to ensure same functionality

--- encrypt.py
+++ encrypt.py
@@ -11,20 +11,6 @@ import math
 def check_coprimality(a, b):
     return bltin_gcd(a, b) == 1
 
-#Function to return an ascii list from a string
-def get_ascii_list(input_string):
-	out = []
-	for i in input_string:
-		out.append(ord(i))
-	return out
-
-#Function to return a string from an ascii list
-def get_string_from_ascii(input_list):
-	out = ""
-	for i in input_list:
-		out += chr(i)	
-	return out
-
 #Class to generate all required numbers for an RSA encryption instance.  Needs the two primes for initialization
 class encryption_set():
 
@@ -96,46 +82,3 @@ class encryption_set():
 		print("K: ", self.k)
 	
 	
-def main():
-	#Sample plain text to encrypt
-	plain_text = "Encryption is challenging, but very cool!"
-	#Variables to store ascii-lists, plain-text lists, etc
-	plain_text_ascii = []
-	cipher_ascii = []
-	cipher_text = ""
-	decrypted_cipher_ascii = []
-	decrypted_cipher_text = ""
-	encrytped_test_list = []
-	decrypted_test_list = []
-	
-	#Create an RSA encryption set object and print out the values it created for itself
-	RSA_object = encryption_set(37, 73)
-	RSA_object.to_String()
-	
-	#Obtain a list of ascii values for the plain text
-	plain_text_ascii = get_ascii_list(plain_text)
-
-	#Encrypt each number in the plain-text ascii list to obtain the cipher-ascii list
-	for i in plain_text_ascii:
-		cipher_ascii.append(int(RSA_object.encrypt_int(i)))
-		
-	#Generate the cipher text from the cipher-ascii list
-	cipher_text = get_string_from_ascii(cipher_ascii)
-	
-	
-	#Decrypt the cipher-ascii.  This should result back to the original plain-ascii list
-	for i in cipher_ascii:
-		decrypted_cipher_ascii.append(RSA_object.decrypt_int(i))
-	
-	#Generate the plain-text from the plain-ascii
-	for i in decrypted_cipher_ascii:
-		decrypted_cipher_text += chr(int(i))
-	
-	#Display the original plain text, cipher text, and the decrypted_cipher_text (which should equal the plain text)
-	print("\n\nPlain text: ", plain_text)
-	print("Cipher text: ", cipher_text)
-	print("Decrypted cipher text: ", decrypted_cipher_text)
-	
-main()
-
-
---
+++ test.py
@@ -0,0 +1,64 @@
+#Program: Simply program to ensure the functionality of the RSA encryption.  Enrypts a plaintext, shows the cipher, and decrypts back to original plain text
+#Author: Aron Schwartz
+#Last edit: 10/21/2019
+
+import encrypt
+
+#Function to return an ascii list from a string
+def get_ascii_list(input_string):
+	out = []
+	for i in input_string:
+		out.append(ord(i))
+	return out
+
+#Function to return a string from an ascii list
+def get_string_from_ascii(input_list):
+	out = ""
+	for i in input_list:
+		out += chr(i)	
+	return out
+
+def main():
+	#Sample plain text to encrypt
+	plain_text = "Can is pretty good at coding, but Aron is WAY better"
+	
+	
+	#Variables to store ascii-lists, plain-text lists, etc
+	plain_text_ascii = []
+	cipher_ascii = []
+	cipher_text = ""
+	decrypted_cipher_ascii = []
+	decrypted_cipher_text = ""
+	encrytped_test_list = []
+	decrypted_test_list = []
+	
+	#Create an RSA encryption set object and print out the values it created for itself
+	RSA_object = encrypt.encryption_set(47, 59)
+	RSA_object.to_String()
+	
+	#Obtain a list of ascii values for the plain text
+	plain_text_ascii = get_ascii_list(plain_text)
+
+	#Encrypt each number in the plain-text ascii list to obtain the cipher-ascii list
+	for i in plain_text_ascii:
+		cipher_ascii.append(int(RSA_object.encrypt_int(i)))
+		
+	#Generate the cipher text from the cipher-ascii list
+	cipher_text = get_string_from_ascii(cipher_ascii)
+	
+	
+	#Decrypt the cipher-ascii.  This should result back to the original plain-ascii list
+	for i in cipher_ascii:
+		decrypted_cipher_ascii.append(RSA_object.decrypt_int(i))
+	
+	#Generate the plain-text from the plain-ascii
+	for i in decrypted_cipher_ascii:
+		decrypted_cipher_text += chr(int(i))
+	
+	#Display the original plain text, cipher text, and the decrypted_cipher_text (which should equal the plain text)
+	print("\n\nPlain text: ", plain_text)
+	print("\nCipher text: ", cipher_text)
+	print("\nDecrypted cipher text: ", decrypted_cipher_text)
+	
+main()
+
